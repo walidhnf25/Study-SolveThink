d1618efe727ce47b91e3f47c7285115086e1bf2f
Walid Hanif Ataullah <walidhnf12@gmail.com>
2025-04-06 21:26:27 +0700

    ubah head title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <title>Pendaftaran Kelas</title>
+    <title>Pendaftaran Kelas Bimbel</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
